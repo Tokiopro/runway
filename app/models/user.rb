4fd449a62f41a11c29ae6a72b2e8f58596bb057e
Tokiopro <tokisaonana@yahoo.co.jp>
2022-06-12 08:13:08 +0000

    User Adminモデル、コントローラー作成

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+         
+  enum age: { male: 0, female: 1, others: 2}
+end
