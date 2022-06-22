6b3beafc0037fad3a8696a48084bdf4bb585c48d
Tokiopro <tokisaonana@yahoo.co.jp>
2022-06-22 12:12:15 +0000

    コメントモデル

--- db/migrate/20220615023858_create_course_types.rb
+++ db/migrate/20220615023858_create_course_types.rb
@@ -1,7 +1,7 @@
 class CreateCourseTypes < ActiveRecord::Migration[6.1]
   def change
     create_table :course_types do |t|
-      t.references :courses, null: false, foreign_key: true
+      t.integer :course_id, null: false
       t.integer :type
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_22_115716) do
+ActiveRecord::Schema.define(version: 2022_06_22_121016) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -99,6 +99,15 @@ ActiveRecord::Schema.define(version: 2022_06_22_115716) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["post_id"], name: "index_courses_on_post_id"
+  end
+
+  create_table "post_comments", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "post_id"
+    t.text "comment"
+    t.string "rate"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "posts", force: :cascade do |t|
