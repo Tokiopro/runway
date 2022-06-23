b84a61140b55490251b74cddb32d81daa5824caa
Tokiopro <tokisaonana@yahoo.co.jp>
2022-06-23 08:30:56 +0000

    コメント機能非同期通信

--- app/controllers/public/post_comments_controller.rb
+++ app/controllers/public/post_comments_controller.rb
@@ -1,23 +1,29 @@
 class Public::PostCommentsController < ApplicationController
-  
+
   def create
     @post = Post.find(params[:post_id])
-    @comment = current_user.comments.new(comment_params)
+    @comment = current_user.post_comments.new(comment_params)
     @comment.post_id = @post.id
     if @comment.save
-      redirect_to post_path(@post), notice: "コメントを投稿しました"
+      flash.now[:notice] = "コメントを投稿しました"
+      #jsファイルをrenderする（非同期通信）
+      render :post_comments
     else
-      render "posts/show"
+      render "error"
     end
   end
-  
+
   def destroy
-    Comment.find_by(id: params[:id], post_id: params[:post_id]).destroy
-    redirect_to post_path(params[:post]), alert: 'コメントを削除しました'
+    #postに紐づいているコメントを削除するため、post_idも必要
+    PostComment.find_by(id: params[:id], post_id: params[:post_id]).destroy
+    flash.now[:alert] = 'コメントを削除しました'
+    #上記で一度post_idをdestroyしてしまっているため、再度@postを定義
+    @post = Post.find(params[:post_id])
+    render :post_comments
   end
-  
+
   private
-  
+
   def comment_params
     params.require(:post_comment).permit(:comment, :rate)
   end
