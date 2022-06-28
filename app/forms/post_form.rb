dc8f15f84682fd25c75199a8a11b14e549c79f4c
Tokiopro <tokisaonana@yahoo.co.jp>
2022-06-28 07:47:10 +0000

    退会機能実装

--- app/forms/post_form.rb
+++ app/forms/post_form.rb
@@ -18,6 +18,8 @@ class  PostForm
     validates :distance
   end
 
+  
+
   def save
     ActiveRecord::Base.transaction do
       post = Post.create(article: article, user_id: user_id, image: image)
@@ -29,4 +31,17 @@ class  PostForm
       CourseMethod.create(method: [])
     end
   end
+
+  def update
+    ActiveRecord::Base.transaction do
+      post = Post.update(article: article, user_id: user_id, image: image)
+      self.post_id = post.id
+      Course.update(post_id: post_id, name: name, distance: distance, prefecutures: prefecutures, undulation: undulation, traffic_light: traffic_light, street_light: street_light)
+      CourseType.update(type: [])
+      CourseTimeZone.update(time_zone: [])
+      CourseEquipment.update(equipment: [])
+      CourseMethod.update(method: [])
+    end
+  end
+
 end
